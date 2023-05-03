a4d5971a79c6d7336bd7b3a603ed99734e7a5169
Thiago Caetano Sobrinho <46784384+caetanosbr@users.noreply.github.com>
2023-05-03 13:04:14 -0300

    v3
    todas as funcionalidades ok

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ const screen1 = document.querySelector(".screen1")
 const screen2 = document.querySelector(".screen2")
 const btnTry = document.querySelector("#btnTry")
 const btnReset = document.querySelector("#btnReset")
-let randomNumber = document.querySelector("#inputNumber")
+const inputNumber = document.querySelector("#inputNumber")
+let randomNumber = Math.round(Math.random() * 10)
 let xAttempts = 1
 
 // Eventos
@@ -15,19 +16,15 @@ document.addEventListener("keydown", handleEnterclick)
 function handleTryClick(event) {
   event.preventDefault() // não faça o padrão
 
-  const inputNumber = document.querySelector("#inputNumber")
-
   if (Number(inputNumber.value) == randomNumber) {
     toggleScreen()
-
-    screen2.querySelector("h2")
-    .innerText = `Acertou em ${xAttempts} tentativas`
-
-  } else if (Number(inputNumber.value) < 0) {
-      alert("Erro o número digitado é menor que 0")
-
-  } else if (Number(inputNumber.value) > 10) {
-      alert("O número digitado é maior que 10")
+    screen2.querySelector("h2").innerText = `Acertou em ${xAttempts} tentativas`
+  } 
+  else if (Number(inputNumber.value) < 0) {
+    alert("O número digitado é menor que 0")
+  }
+  else if (Number(inputNumber.value) > 10) {
+    alert(" O número digitado é maior que 10")
   }
 
   inputNumber.value = ""
